716a0174147b7b904976ba0e487f3228e4a30b18
ldex <laurent.duveau+git@gmail.com>
2023-11-17 21:04:28 -0500

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withDebugTracing,
+  withPreloading,
+  withViewTransitions
+} from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withDebugTracing(),
+      withViewTransitions()
+    ),
+	provideClientHydration()
+  ],
 };
